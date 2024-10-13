4d3a1d56cacc450f2da8c9c20b5124a8fc240f40
khanhky <khanh.ky@gmail.com>
2024-10-13 15:15:31 -0500

    integer

--- model/item.go
+++ model/item.go
@@ -8,8 +8,8 @@ type Item struct {
 	Type string `json:"Type" gorm:"null;column:Type;size:255"`
 	Category string `json:"Category" gorm:"null;column:Category;size:20"`
 	Description string `json:"Description" gorm:"null;column:Category;size:255"`	
-	Inventory Int `json:"Inventory" gorm:"null;column:Inventory"`
-	Qty_per_Box Int `json:"Qty_per_Box" gorm:"null;column:Qty_per_Box"`
+	Inventory int `json:"Inventory" gorm:"null;column:Inventory"`
+	Qty_per_Box int `json:"Qty_per_Box" gorm:"null;column:Qty_per_Box"`
 	Average_Cost float64 `json:"Average_Cost" gorm:"null;column:Average_Cost"`	
 	Average_Price float64 `json:"Average_Price" gorm:"null;column:Average_Price"`		
 }
