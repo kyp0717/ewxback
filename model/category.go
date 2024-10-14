5e27981c251dfd95cba2364ccbf65acbb0a1f3b4
khanhky <khanh.ky@gmail.com>
2024-10-13 21:22:06 -0500

    model code

--- model/category.go
+++ model/category.go
@@ -1,7 +1,7 @@
 package model
 
 type Category struct {
-	Category string `json:"Category" gorm:"primaryKey";size:20"`
+	Category string `json:"Category" gorm:"primaryKey;size:20"`
 
 	Category_Description string `json:"Category_Description" gorm:"null;column:Category_Description;size:255"`
 	Reserved_Percentage_BySale  float64 `json:"Reserved_Percentage_BySale" gorm:"null;column:Reserved_Percentage_BySale"`
